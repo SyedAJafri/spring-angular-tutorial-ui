977f24bec70d454be368ed79f32a8198dc66f330
SyedAJafri <Syedjafri99@gmail.com>
2016-05-06 15:05:46 -0400

    Populating home fields with data from resource controller

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -1,4 +1,9 @@
 angular.module('hello', [])
-    .controller('home', function ($scope) {
-        $scope.greeting = {id: 'xxx', content: 'Hello World'}
+    .controller('home', function ($http) {
+        var self = this;
+        //make an http request to /resource/ then set greeting to the data
+        //index.html home fields such as home.greeting and home.id should be populated
+        $http.get('/resource/').then(function (response) {
+            self.greeting = response.data;
+        });
     })
